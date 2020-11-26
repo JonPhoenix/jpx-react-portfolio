4912cdd876b8895d383b6f56b749593804ecfcb3
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-25 22:21:07 -0800

    Updates pages and components and shapes Navbar

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+// import css styles
+import "../../css/style.css";
 
 function Footer() {
   return (
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+// import css styles
+import "../../css/style.css";
 
 function Navbar() {
   return (
     <>
-      <div></div>
+      <div>
+        <div className="container">
+          <header className="row">
+            <nav className="col">
+              <ul className="nav justify-content-start transition">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Jon Phoenix
+                    <sup>
+                      <span class="note">®</span>
+                    </sup>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/work">
+                    Work
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    href="./assets/doc/resume.pdf"
+                    download=""
+                  >
+                    Resume
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+        </div>
+      </div>
     </>
   );
 }
