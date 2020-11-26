8a731de689b00d42e8076753de8d2d6719b3c3d5
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-25 18:38:10 -0800

    pseudo code imports and bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import React
+import React from "react";
+// import React-router-dom / BrowseRouter as Router / Route / Switch
 
+// import Components
+// Navbar
+// Footer
+
+// import Pages
+// import HomePage
+// import ProjectsPage
+
+// import CSS style
+import "./App.css";
+
+// App navigation with React Router
 function App() {
   return (
     <div className="App">
