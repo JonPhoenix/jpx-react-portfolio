b569e2cef7d0ec9f72f491a300c0ea9c54946e04
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-27 22:46:20 -0800

    Creates a basic home page

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,7 +5,35 @@ import "../../css/style.css";
 function HomePage() {
   return (
     <>
-      <div></div>
+      <div className="container">
+        <div className="row">
+          <section className="col-12 text-center">
+            <p className="title">Hello</p>
+            <p>I am Jon Phoenix</p>
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row section">
+          <section className="col-12 text-center">
+            <article className="paragraph">
+              <figure>
+                <img
+                  src="./assets/img/jon-photo.jpg"
+                  className="jon-photo"
+                  alt="Jon Phoenix"
+                />
+              </figure>
+              <p className="title">Portfolio</p>
+              <p>
+                Welcome to Jon Phoenix's online portfolio, a selection of web
+                applications featuring JavaScript, HTML, CSS, Databases, React
+                and responsive UI design across viewports.
+              </p>
+            </article>
+          </section>
+        </div>
+      </div>
     </>
   );
 }
