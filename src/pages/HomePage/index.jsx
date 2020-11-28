05108c376500fd4c0b84c6879ff207dfa2f422b1
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-27 23:06:12 -0800

    Updates about and images

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -16,20 +16,27 @@ function HomePage() {
       <div className="container">
         <div className="row section">
           <section className="col-12 text-center">
-            <article className="paragraph">
+            <article className="about">
               <figure>
                 <img
                   src="./assets/img/jon-photo.jpg"
-                  className="jon-photo"
+                  className="about-photo"
                   alt="Jon Phoenix"
                 />
               </figure>
-              <p className="title">Portfolio</p>
-              <p>
+              <p className="title">about me</p>
+              <p className="paragraph">
                 Welcome to Jon Phoenix's online portfolio, a selection of web
                 applications featuring JavaScript, HTML, CSS, Databases, React
                 and responsive UI design across viewports.
               </p>
+              <figure>
+                <img
+                  src="./assets/img/rainier_photo.jpg"
+                  className="about-photo"
+                  alt="Seattle"
+                />
+              </figure>
             </article>
           </section>
         </div>
