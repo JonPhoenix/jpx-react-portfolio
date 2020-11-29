3b0d341b0812993870218f144ba5b622a8e54957
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-28 20:25:00 -0800

    Updates homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -16,28 +16,30 @@ function HomePage() {
       </div>
       <div className="container">
         <div className="row section">
-          <section className="col-12 text-center">
+          <section className="col-lg-12 text-center">
             <article className="about">
-              <figure>
-                <img
-                  src="./assets/img/jon-photo.jpg"
-                  className="about-photo"
-                  alt="Jon Phoenix"
-                />
-              </figure>
-              <p className="title">about me</p>
+              <div className="row">
+                <figure className="col-lg-6">
+                  <img
+                    src="./assets/img/jon-photo.jpg"
+                    className="about-photo"
+                    alt="Jon Phoenix"
+                  />
+                </figure>
+                <figure className="col-lg-6">
+                  <img
+                    src="./assets/img/rainier_photo.jpg"
+                    className="about-photo"
+                    alt="Seattle"
+                  />
+                </figure>
+              </div>
+              <p className="title col-lg-12 text-center">about me</p>
               <p className="paragraph">
                 Welcome to Jon Phoenix's online portfolio, a selection of web
                 applications featuring JavaScript, HTML, CSS, Databases, React
                 and responsive UI design across viewports.
               </p>
-              <figure>
-                <img
-                  src="./assets/img/rainier_photo.jpg"
-                  className="about-photo"
-                  alt="Seattle"
-                />
-              </figure>
             </article>
           </section>
         </div>
